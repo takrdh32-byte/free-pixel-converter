510b93a1d70d28f54075951e4c78307a695b38bb
takrdh32-byte <takrdh32@gmail.com>
2026-06-06 16:21:38 +0530

    Create android/app/src/main/kotlin/com/example/free_pixel_converter/MainActivity.kt

---
+++ android/app/src/main/kotlin/com/example/free_pixel_converter/MainActivity.kt
@@ -0,0 +1,7 @@
+package com.example.free_pixel_converter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+    // इसे बिल्कुल खाली रखें, यह अपने आप v2 आर्किटेक्चर लोड कर लेगा
+}

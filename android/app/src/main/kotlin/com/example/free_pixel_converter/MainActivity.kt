3f418ee74739a83fc3bfdb87ff9cbe3fd2623a97
takrdh32-byte <takrdh32@gmail.com>
2026-06-06 16:29:28 +0530

    Delete MainActivity.kt

--- android/app/src/main/kotlin/com/example/free_pixel_converter/MainActivity.kt
+++
@@ -1,7 +0,0 @@
-package com.example.free_pixel_converter
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-    // इसे बिल्कुल खाली रखें, यह अपने आप v2 आर्किटेक्चर लोड कर लेगा
-}

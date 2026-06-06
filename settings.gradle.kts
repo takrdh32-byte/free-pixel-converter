817afd97e9bb8d7c363eb0cebcbc705557cdf7a2
takrdh32-byte <takrdh32@gmail.com>
2026-06-06 18:38:28 +0530

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -6,6 +6,15 @@ pluginManagement {
     }
 }
 
+// यहाँ हम बिना किसी फाइल के सीधे कोड से AndroidX चालू कर रहे हैं
+gradle.settingsEvaluated {
+    val startParameter = gradle.startParameter
+    startParameter.projectProperties = startParameter.projectProperties + mapOf(
+        "android.useAndroidX" to "true",
+        "android.enableJetifier" to "true"
+    )
+}
+
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {

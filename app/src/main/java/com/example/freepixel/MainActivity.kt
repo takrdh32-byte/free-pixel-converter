56529fee448fc9ce1e4b8c2efba5eb860098889f
takrdh32-byte <takrdh32@gmail.com>
2026-06-06 17:33:01 +0530

    Create app/src/main/java/com/example/freepixel/MainActivity.kt

---
+++ app/src/main/java/com/example/freepixel/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.freepixel
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        // यहाँ से आपका कोटलिन कोड शुरू होगा
+    }
+}

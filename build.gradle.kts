505ed1a6f507f0a747baa1979bff12c4347e4cdb
takrdh32-byte <takrdh32@gmail.com>
2026-06-06 17:47:47 +0530

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,3 @@
-// प्लगइन्स को ढूँढने के लिए रिपॉजिटरी सेट करना
-pluginManagement {
-    repositories {
-        google()
-        mavenCentral()
-        gradlePluginPortal()
-    }
-}
-
 plugins {
     id("com.android.application") version "8.2.2" apply false
     id("org.jetbrains.kotlin.android") version "1.9.22" apply false
